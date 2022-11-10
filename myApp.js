9e59d06c6427d2601e923b7b80481b27f4d5aa70
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-11-10 10:22:30 -0800

    Add library route to myApp.js. Also added explanatory comments.
    Rename library route to name.

--- myApp.js
+++ myApp.js
@@ -2,17 +2,21 @@ let express = require("express");
 require("dotenv").config();
 let app = express();
 
+// Middleware example.
 app.use("/", function(req, res, next) {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
+// Configure serving static files.
 app.use("/public", express.static("public"));
 
+// Root route sends index.html file.
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+// Send json. Vary based on .env variables.
 app.get("/json", function(req, res) {
   if (process.env.MESSAGE_STYLE === "uppercase") {
     res.json({ message: "HELLO JSON" });
@@ -21,6 +25,7 @@ app.get("/json", function(req, res) {
   }
 });
 
+// Middlewares can be chained.
 app.get(
   "/now",
   function(req, res, next) {
@@ -32,8 +37,14 @@ app.get(
   }
 );
 
+// Use route parameters to get specifics about request.
 app.get("/:word/echo", function(req, res) {
   res.json({ echo: req.params.word });
+});
+
+// Use query strings to get specifics about request.
+app.get("/name", function(req, res) {
+  res.json({ name: `${req.query.first} ${req.query.last}` });
 });
 
 module.exports = app;
