115425b7ce3a803333931c19198cc0236164434c
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-11-14 13:36:11 -0800

    Add POST request handler on name route.

--- myApp.js
+++ myApp.js
@@ -51,4 +51,9 @@ app.get("/name", function(req, res) {
   res.json({ name: `${req.query.first} ${req.query.last}` });
 });
 
+// Use body-parser to access body of POST request.
+app.post("/name", function(req, res) {
+  res.json({ name: `${req.body.first} ${req.body.last}` });
+});
+
 module.exports = app;
